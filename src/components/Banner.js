caa3e021ea40a6f27613ff30eccd5893e7f0fb7c
Zeta <ag.sebastian912@gmail.com>
2024-06-24 17:43:35 -0300

    Updates

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -4,7 +4,41 @@ import { ArrowRightCircle } from "react-bootstrap-icons";
 import headerImg from "../assets/img/header-img.svg"
 
 export const Banner =() =>{
-    const toRotate = [ "Desarrollo Web", "Diseño Web", "SEO"]
+    const [loopNum, setLoopNum] = useState(0);
+    const [isDeleting, setIsDeleting] = useState(false);
+    const toRotate = [ "Desarrollo Web", "Diseño Web", "SEO"];
+    const [text, setText] = useState('');
+    const [delta, setDelta] = useState(300 - Math.random() * 100);
+    const period = 2000;
+
+    useEffect(() => {
+        let ticker = setInterval(() => {
+            ticker();
+        }, delta)
+
+        return() => {clearInterval(ticker)};
+    } [text])
+
+    const tick = () => {
+        let i = loopNum % toRotate.length;
+        let fullText = toRotate[i];
+        let updatedText = isDeleting ? fullText.substring(0, text.length - 1) : fullText.substring(0, text.length +1);
+
+        setText(updatedText);
+
+        if(isDeleting) {
+            setDelta(prevDelta => prevDelta /2)
+        }
+
+        if (!isDeleting && updatedText === fullText) {
+            setIsDeleting(true);
+            setDelta(period);
+        } else if(isDeleting && updatedText === '') {
+            setIsDeleting(false);
+            setLoopNum(loopNum + 1);
+            setDelta(500);
+        }
+    }
 
     return (
         <section className="banner" id="home">
@@ -12,7 +46,7 @@ export const Banner =() =>{
                 <Row className="align-items-center">
                     <Col xs={12} md={6} xl={7}>
                     <span className="tagline">Welcome to our Portfolio</span>
-                    <h1>{'Somos Z Agency'}<span className="wrap">Desarrollo Web</span> </h1>
+                    <h1>{'Somos Z Agency'}<span className="wrap">{text}/span> </h1>
                     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi ipsum ante, bibendum nec mi a,
                     varius aliquet turpis. Mauris sit amet justo eget diam consequat sodales.</p>
                     <button onClick={() => console.log('connect')}>Let's Connect <ArrowRightCircle size={25}/></button>
