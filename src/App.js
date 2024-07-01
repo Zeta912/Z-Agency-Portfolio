259b685284e54a4c05317d4f731eaa7320fb1c75
Zeta <ag.sebastian912@gmail.com>
2024-07-01 02:46:29 -0300

    Project restart

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import Navbar from './components/navbar';
+import Hero from './components/hero';
+import './App.css';
+
+function App() {
+  return (
+    <main>
+      <Navbar />
+      <Hero />
+    </main>
+  );
+}
+
+export default App;
